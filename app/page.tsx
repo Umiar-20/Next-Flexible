e2c98548e8abc775e34b259ee8fc2cc1a156da1f
Umiar-20 <swaso.awijaya@gmail.com>
2024-07-16 13:06:49 +0700

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <div>ini page Home</div>;
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load More</h1>
+    </section>
+  );
 }
 
 export default Home;
